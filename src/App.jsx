c3c6b196b3313d422a28ecf9f77d7e53887e55c9
Charindu <ruhansamunasinghe@gmail.com>
2025-10-14 14:33:52 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Videos from './Components/Videos/Videos'
-import Testimonial from './Components/Testimonial/Testimonial'
-import Contact from './Components/Contact/Contact'
-import { ToastContainer } from 'react-toastify'
-import Footer from './Components/Footer/Footer'
-import Preloader from './Components/Preloader/Preloader'
+import React, { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
+import Videos from './Components/Videos/Videos';
+import Testimonial from './Components/Testimonial/Testimonial';
+import Contact from './Components/Contact/Contact';
+import { ToastContainer } from 'react-toastify';
+import Footer from './Components/Footer/Footer';
+import Preloader from './Components/Preloader/Preloader';
 
 const App = () => {
-
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2200);
+  useEffect(() => {
+    const handleLoad = () => {
+      // add small delay for smooth transition
+      setTimeout(() => setLoading(false), 500);
+    };
 
-    return () => clearTimeout(timer);
-  },[])
-
+    if (document.readyState === 'complete') {
+      handleLoad(); // already loaded
+    } else {
+      window.addEventListener('load', handleLoad);
+      return () => window.removeEventListener('load', handleLoad);
+    }
+  }, []);
 
   return (
     <div>
@@ -40,6 +44,6 @@ const App = () => {
       )}
     </div>
   );
-}
+};
 
-export default App
+export default App;
