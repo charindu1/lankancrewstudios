fb446f92782d52ed309686c281f262bf77ff88a1
Charindu <ruhansamunasinghe@gmail.com>
2025-10-14 14:13:45 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   useEffect(()=>{
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2200);
+    }, 5000);
 
     return () => clearTimeout(timer);
   },[])
