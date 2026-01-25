3b8dcab3c1ba9f8d60578d0d009dc3eafb8589b8
charindu1 <charinduruhansa@gmail.com>
2026-01-25 20:50:12 +0530

    fix

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import './Hero.css'
 import big_logo from '../../assets/big_logo.png'
 import { IoMdArrowRoundForward } from "react-icons/io";
 import { Link } from 'react-scroll';
+import { Helmet } from 'react-helmet-async';
 
 const Hero = () => {
 
@@ -18,6 +19,12 @@ const Hero = () => {
 
   return (
     <>
+      <Helmet>
+        <title>Lankan Crew - Home</title>
+        <meta name="description" content="Lankan Crew - Discover, thrilling and entertaining thriller, horror, mystery, sci-fi, and comedy short films in Sri Lanka." />
+        <meta name="keywords" content="Lankan Crew, Sri Lanka, short films, thriller, horror, mystery, sci-fi, comedy, films" />
+        <link rel="canonical" href="/" />
+      </Helmet>
       <div className='hero container'>
         <div className="hero-left">
           <h1>Wellcome To <span>Lankan Crew</span></h1>
